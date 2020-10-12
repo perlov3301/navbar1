3ee14386a76cf7b8f5df6001b4ecaec3f61d8785
Alexander Perlov <alexanderperlov@Alexanders-MacBook-Air.local>
2020-10-12 20:24:00 +0300

    toggle background of active page title

--- navbarflexhamburger.js
+++ navbarflexhamburger.js
@@ -3,9 +3,22 @@ document.onreadystatechange = function () {
 			  console.log("is interactive");
         const toggleButton = document.getElementsByClassName('toggle-button')[0];
         const navbarLinks = document.getElementsByClassName('navbar-links')[0];
+        const liarray = document.getElementsByClassName('navbarli');
         // console.log(navbarLinks);
+        // console.log(liarray);
         toggleButton.addEventListener('click', () => {
-           navbarLinks.classList.toggle('active'); // added or removed class
+           navbarLinks.classList.toggle('active1'); // added or removed class
         });
+        for (let i = 0; i < liarray.length; i++) {
+            liarray[i].addEventListener('click', function (e) {
+                // e.preventDefault();
+                liarray[i].classList.add('bgcolor');
+                for (let j = 0; j < liarray.length; j++) {
+                    if (j !== i) {
+                        liarray[j].classList.remove('bgcolor');
+                    }
+                }
+            });
+        }
     }
 };
